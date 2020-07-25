d998536c29c25a85b62ac189bb6ee3713982e43f
kacimcbrayer <kacimcbrayer@gmail.com>
2020-07-26 03:06:57 +0900

    Your Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, IterableDiffers } from '@angular/core';
+import { templateJitUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>
+      Welcome to {{title}}!
+    </h1>
+
+    <app-input-button-unit></app-input-button-unit>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todo-list';
 }
